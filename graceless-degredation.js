0e5a1ee5dcc197cf3e7cc6c8ad87d194d9004ee3
Dan Rench <drench+github@gmail.com>
2010-12-31 12:49:49 -0600

    Emulate <blink> with setInterval

--- graceless-degredation.js
+++ graceless-degredation.js
@@ -4,18 +4,23 @@ jQuery(document).ready(function () {
 
     jQuery('hr').replaceWith('<div><img width="100%" src="' + base + 'img/colorbar.gif"></div>');
 
-    jQuery('<img src="' + base + 'img/mail/' + Math.floor(Math.random() * 7) + '.gif>').insertBefore('a[href^="mailto:"]');
-
-    var blinkify = function (el) {
-        var o = el.innerHTML;
-        el.innerHTML = '<blink>' + o + '</blink>';
-    };
+//    jQuery('<img src="' + base + 'img/mail/' + Math.floor(Math.random() * 7) + '.gif>').insertBefore('a[href^="mailto:"]');
 
     var blinkers = ['b', 'i', 'strong', 'u'];
+    var blinks = [];
 
     for (var i=0,t; t=blinkers[i]; ++i) {
-        var tgs = document.getElementsByTagName(t);
-        for (var j=0,el; el=tgs[j]; ++j) blinkify(el);
+        var bs = document.getElementsByTagName(t);
+        for (var j=0,b; b=bs[j]; ++j) {
+            blinks.push(b);
+        }
     }
+
+    window.setInterval(function () {
+        for (var i=0,b; b=blinks[i]; ++i) {
+            b.style.visibility =
+                (b.style.visibility == 'hidden') ? '' : 'hidden';
+        }
+    }, 1000);
 
 });
