d7c8ef7608730661597900351d5db4c59575df01
Dan Rench <drench+github@gmail.com>
2010-12-31 12:28:42 -0600

    Aw, IE6 doesn't implement <blink>

--- graceless-degredation.js
+++ graceless-degredation.js
@@ -1,5 +1,21 @@
 jQuery(document).ready(function () {
 
-    jQuery('hr').replaceWith('<div><img width="100%" src="/wp-content/plugins/graceless-degredation/img/colorbar.gif"></div>');
+    var base = "/wp-content/plugins/graceless-degredation/";
+
+    jQuery('hr').replaceWith('<div><img width="100%" src="' + base + 'img/colorbar.gif"></div>');
+
+    jQuery('<img src="' + base + 'img/mail/' + Math.floor(Math.random() * 7) + '.gif>').insertBefore('a[href^="mailto:"]');
+
+    var blinkify = function (el) {
+        var o = el.innerHTML;
+        el.innerHTML = '<blink>' + o + '</blink>';
+    };
+
+    var blinkers = ['b', 'i', 'strong', 'u'];
+
+    for (var i=0,t; t=blinkers[i]; ++i) {
+        var tgs = document.getElementsByTagName(t);
+        for (var j=0,el; el=tgs[j]; ++j) blinkify(el);
+    }
 
 });
